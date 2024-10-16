1558783714d52cedc340411cf95e11851fa45d0b
snehal r wadhai <snehalwadhai7@gmail.com>
2024-10-16 07:13:03 +0530

    added changes in Sum.java file

--- Sum.java
+++ Sum.java
@@ -2,7 +2,7 @@ public class Sum {
 
 public static void main(String args[])   
 {  
-int n1 = 225, n2 = 115, sum;  
+int n1 = 22, n2 = 115, sum;  
 sum = n1 + n2;  
 System.out.println("The sum of numbers is: "+sum);  
 
